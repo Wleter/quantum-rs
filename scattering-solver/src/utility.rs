b9d7448e19dda7b1d0b3e7f5aa7c49402bd5b767
Wleter <marcraftzastepczy@gmail.com>
2025-01-21 12:02:34 +0100

    fix non symmetric inverse

--- scattering-solver/src/utility.rs
+++ scattering-solver/src/utility.rs
@@ -1,8 +1,7 @@
 use std::{fs::{create_dir_all, File}, io::Write, path::Path};
-use mat::AsMatMut;
 use serde::Serialize;
 
-use faer::{dyn_stack::{MemBuffer, MemStack}, linalg::{self, cholesky, lu, temp_mat_scratch, temp_mat_uninit}, perm::PermRef, prelude::*, unzip, zip};
+use faer::{dyn_stack::{MemBuffer, MemStack}, linalg::{self, cholesky, lu, temp_mat_scratch, temp_mat_uninit, temp_mat_zeroed}, perm::PermRef, prelude::*, unzip, zip};
 
 #[derive(Clone, Copy, Debug)]
 pub struct AngMomentum(pub u32);
@@ -68,115 +67,112 @@ pub fn save_serialize(
     Ok(())
 }
 
-
-pub fn inverse_inplace(mat: MatRef<f64>, mut out: MatMut<f64>, perm: &mut [usize], perm_inv: &mut [usize]) {
+pub fn inverse_inplace(
+    mat: MatRef<f64>,
+    mut out: MatMut<f64>,
+    perm: &mut [usize],
+    perm_inv: &mut [usize],
+) {
     assert!(mat.nrows() == mat.ncols());
     let dim: usize = mat.nrows();
-    zip!(out.as_mut(), mat)
-        .for_each(|unzip!(o, m)| *o = *m);
+    zip!(out.as_mut(), mat).for_each(|unzip!(o, m)| *o = *m);
 
     lu::partial_pivoting::factor::lu_in_place(
-        out.as_mut(), 
-        perm, 
-        perm_inv, 
-        faer::Par::Seq, 
+        out.as_mut(),
+        perm,
+        perm_inv,
+        faer::Par::Seq,
         MemStack::new(&mut MemBuffer::new(
             lu::partial_pivoting::factor::lu_in_place_scratch::<usize, f64>(
-                dim, 
+                dim,
                 dim,
                 faer::Par::Seq,
                 Default::default(),
-            )
+            ),
         )),
-        default()
+        default(),
     );
 
-    let mut buffer = MemBuffer::new(
-        temp_mat_scratch::<f64>(dim, dim)
-            .and(temp_mat_scratch::<f64>(dim, dim))
-    );
+    let mut buffer = MemBuffer::new(temp_mat_scratch::<f64>(dim, dim)
+        .and(temp_mat_scratch::<f64>(dim, dim)));
     let mut stack = MemStack::new(&mut buffer);
 
-    let perm_ref = unsafe {
-        PermRef::new_unchecked(perm, perm_inv, dim)
-    };
-
     let (mut l, stack) = unsafe { temp_mat_uninit::<f64, _, _>(dim, dim, &mut stack) };
-    let mut l = l.as_mat_mut();
+    let mut l = mat::AsMatMut::as_mat_mut(&mut l);
 
-    let (mut u, _) = unsafe { temp_mat_uninit::<f64, _, _>(dim, dim, stack) };
-    let mut u = u.as_mat_mut();
+    let (mut u, _) = temp_mat_zeroed::<f64, _, _>(dim, dim, stack);
+    let mut u = mat::AsMatMut::as_mat_mut(&mut u);
 
-    u.as_mat_mut().copy_from_triangular_upper(&out);
+    u.copy_from_triangular_upper(&out);
 
+    zip!(&mut l, &out).for_each_triangular_lower(linalg::zip::Diag::Skip, |unzip!(l, o)| *l = *o);
     zip!(&mut l).for_each_triangular_upper(linalg::zip::Diag::Skip, |unzip!(x)| *x = 0.);
     l.as_mut().diagonal_mut().fill(1.);
+
+    println!("{l:?}");
+
+    let perm_ref = unsafe { PermRef::new_unchecked(perm, perm_inv, dim) };
 
     lu::partial_pivoting::inverse::inverse(
         out.as_mut(),
         l.as_ref(),
         u.as_ref(),
-        perm_ref, 
+        perm_ref,
         faer::Par::Seq,
         MemStack::new(&mut MemBuffer::new(
-            lu::partial_pivoting::inverse::inverse_scratch::<usize, f64>(
-                dim, 
-                Par::Seq,
-            )
-        ))
+            lu::partial_pivoting::inverse::inverse_scratch::<usize, f64>(dim, faer::Par::Seq),
+        )),
     );
 }
 
 /// Should be faster for symmetric matrices but did not observed that.
-pub fn inverse_symmetric_inplace(mat: MatRef<f64>, mut out: MatMut<f64>, perm: &mut [usize], perm_inv: &mut [usize]) {
+pub fn inverse_symmetric_inplace(
+    mat: MatRef<f64>,
+    mut out: MatMut<f64>,
+    perm: &mut [usize],
+    perm_inv: &mut [usize],
+) {
     let dim: usize = mat.nrows();
 
     let mut buffer = MemBuffer::new(
         temp_mat_scratch::<f64>(dim, 1)
             .and(temp_mat_scratch::<f64>(dim, 1))
-            .and(temp_mat_scratch::<f64>(dim, dim))
+            .and(temp_mat_scratch::<f64>(dim, dim)),
     );
     let mut stack = MemStack::new(&mut buffer);
     let (mut sub_diag, stack) = unsafe { temp_mat_uninit::<f64, _, _>(dim, 1, &mut stack) };
-    let mut sub_diag = sub_diag.as_mat_mut()
-        .col_mut(0)
-        .as_diagonal_mut();
+    let mut sub_diag = mat::AsMatMut::as_mat_mut(&mut sub_diag).col_mut(0).as_diagonal_mut();
 
     let (mut diag, stack) = unsafe { temp_mat_uninit::<f64, _, _>(dim, 1, stack) };
-    let mut diag = diag.as_mat_mut()
-        .col_mut(0)
-        .as_diagonal_mut();
+    let mut diag = mat::AsMatMut::as_mat_mut(&mut diag).col_mut(0).as_diagonal_mut();
 
     let (mut l, _) = unsafe { temp_mat_uninit::<f64, _, _>(dim, dim, stack) };
-    let mut l = l.as_mat_mut();
+    let mut l = mat::AsMatMut::as_mat_mut(&mut l);
 
-    zip!(&mut l, &mat)
-        .for_each(|unzip!(l, m)| *l = *m);
+    zip!(&mut l, &mat).for_each(|unzip!(l, m)| *l = *m);
 
     cholesky::bunch_kaufman::factor::cholesky_in_place(
-        l.as_mut(), 
-        sub_diag.as_mut(), 
-        Default::default(), 
-        perm, 
-        perm_inv, 
-        faer::Par::Seq, 
+        l.as_mut(),
+        sub_diag.as_mut(),
+        Default::default(),
+        perm,
+        perm_inv,
+        faer::Par::Seq,
         MemStack::new(&mut MemBuffer::new(
             cholesky::bunch_kaufman::factor::cholesky_in_place_scratch::<usize, f64>(
                 dim,
-                Par::Seq,
+                faer::Par::Seq,
                 Default::default(),
-            )
+            ),
         )),
-        Default::default()
+        Default::default(),
     );
 
     diag.copy_from(l.as_ref().diagonal());
     l.as_mut().diagonal_mut().fill(1.);
     zip!(&mut l).for_each_triangular_upper(linalg::zip::Diag::Skip, |unzip!(x)| *x = 0.);
 
-    let perm_ref = unsafe {
-        PermRef::new_unchecked(perm, perm_inv, dim)
-    };
+    let perm_ref = unsafe { PermRef::new_unchecked(perm, perm_inv, dim) };
 
     cholesky::bunch_kaufman::inverse::inverse(
         out.as_mut(),
@@ -186,16 +182,7 @@ pub fn inverse_symmetric_inplace(mat: MatRef<f64>, mut out: MatMut<f64>, perm: &
         perm_ref,
         Par::Seq,
         MemStack::new(&mut MemBuffer::new(
-            cholesky::bunch_kaufman::inverse::inverse_scratch::<usize, f64>(
-                dim,
-                Par::Seq,
-            )
+            cholesky::bunch_kaufman::inverse::inverse_scratch::<usize, f64>(dim, faer::Par::Seq),
         )),
     );
-
-    // for j in 0..dim {
-    //     for i in 0..j {
-    //         out[(j, i)] = out[(i, j)];
-    //     }
-    // }
 }
