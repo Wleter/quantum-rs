1ea682b8294be2f5cfe5a334cd92cbfb74963687
Wleter <marcraftzastepczy@gmail.com>
2025-04-04 08:19:52 +0200

    only change step by 2 of the previous one in log derivative

--- scattering-solver/src/log_derivatives.rs
+++ scattering-solver/src/log_derivatives.rs
@@ -6,7 +6,7 @@ use std::marker::PhantomData;
 use faer::{linalg::matmul::matmul, prelude::{c64, SolverCore}, unzipped, zipped, Mat, MatMut, MatRef};
 use quantum::utility::{ratio_riccati_i_deriv, ratio_riccati_k_deriv, riccati_j_deriv, riccati_n_deriv};
 
-use crate::{boundary::{Boundary, Direction}, numerovs::{propagator_watcher::PropagatorWatcher, StepRule}, observables::s_matrix::SMatrix, propagator::{Equation, Propagator, Repr, Solution}, utility::{inverse_inplace, inverse_inplace_nodes}};
+use crate::{boundary::{Boundary, Direction}, numerovs::{propagator_watcher::PropagatorWatcher, StepRule}, observables::s_matrix::SMatrix, propagator::{Equation, Propagator, Repr, Solution}, utility::inverse_inplace_nodes};
 
 
 #[derive(Clone, Copy, Debug, Default)]
@@ -71,13 +71,17 @@ where
         self.solution.s_matrix(&self.equation)
     }
 
+    pub fn solution(&self) -> &Solution<LogDeriv<Mat<f64>>> {
+        &self.solution
+    }
+
     fn step_r_target(&mut self, r: Option<f64>) {
         self.equation.buffer_w_matrix(self.solution.r);
-        self.solution.dr = match self.direction {
+        self.solution.dr = (match self.direction {
             Direction::Inwards => -self.step_rule.get_step(&self.equation.buffered_w_matrix()),
             Direction::Outwards => self.step_rule.get_step(&self.equation.buffered_w_matrix()),
             Direction::Step(dr) => dr,
-        };
+        }).clamp(0.0, 2. * self.solution.dr);
 
         if let Some(r) = r {
             if (self.solution.r - r).abs() < self.solution.dr.abs() {
@@ -130,13 +134,12 @@ struct LogDerivativeStep<R: LogDerivativeReference> {
     buffer2: Mat<f64>,
     buffer3: Mat<f64>,
 
-    z_matrix: Mat<f64>,
     w_ref: Mat<f64>,
 
     perm: Vec<usize>,
     perm_inv: Vec<usize>,
 
-    reference: PhantomData<R>
+    reference: PhantomData<R>,
 }
 
 impl<R: LogDerivativeReference> LogDerivativeStep<R> {
@@ -145,7 +148,6 @@ impl<R: LogDerivativeReference> LogDerivativeStep<R> {
             buffer1: Mat::zeros(size, size),
             buffer2: Mat::zeros(size, size),
             buffer3: Mat::zeros(size, size),
-            z_matrix: Mat::zeros(size, size),
             w_ref: Mat::zeros(size, size),
             perm: vec![0; size],
             perm_inv: vec![0; size],
@@ -157,15 +159,39 @@ impl<R: LogDerivativeReference> LogDerivativeStep<R> {
     fn perform_step(&mut self, sol: &mut Solution<LogDeriv<Mat<f64>>>, eq: &Equation<Mat<f64>>) {
         let h = sol.dr / 2.0;
 
-        eq.w_matrix(sol.r + h, &mut self.buffer1);
-        R::w_ref(self.buffer1.as_ref(), self.w_ref.as_mut());
+        eq.w_matrix(sol.r + h, &mut self.buffer3);
+        R::w_ref(self.buffer3.as_ref(), self.w_ref.as_mut());
 
-        zipped!(self.buffer1.as_mut(), eq.unit.as_ref())
-        .for_each(|unzipped!(b, u)| {
-            *b = u + h * h / 6. * *b // different sign since W(c) is -W(c) in our notation
+        eq.w_matrix(sol.r, &mut self.buffer1);
+
+        R::imbedding1(h, self.w_ref.as_ref(), self.buffer2.as_mut());
+
+        zipped!(self.buffer2.as_mut(), self.buffer1.as_ref(), self.w_ref.as_ref())
+        .for_each(|unzipped!(y1, w_a, w_ref)| {
+            *y1 = *y1 + h / 3. * (w_ref - w_a) // sign change because of different convention
+        });
+        // buffer2 is a y_1(a, c)
+
+        zipped!(self.buffer2.as_mut(), sol.sol.0.as_ref())
+        .for_each(|unzipped!(y1, sol)| {
+            *y1 = sol + *y1
+        });
+        sol.nodes += inverse_inplace_nodes(self.buffer2.as_ref(), sol.sol.0.as_mut(), &mut self.perm, &mut self.perm_inv);
+        // sol is now (Y(a) + y_1(a, c))^-1
+
+        R::imbedding3(h, self.w_ref.as_ref(), self.buffer1.as_mut());
+        matmul(self.buffer2.as_mut(), self.buffer1.as_ref(), sol.sol.0.as_ref(), None, 1.0, faer::Parallelism::None);
+        R::imbedding2(h, self.w_ref.as_ref(), self.buffer1.as_mut());
+        matmul(sol.sol.0.as_mut(), self.buffer2.as_ref(), self.buffer1.as_ref(), None, 1.0, faer::Parallelism::None);
+        // sol is a second term in y_3 (Y(a) + y_1(a, c))^-1 y_2
+
+        zipped!(self.buffer3.as_mut(), eq.unit.as_ref(), self.w_ref.as_ref())
+        .for_each(|unzipped!(b, u, w)| {
+            *b = u - h * h / 6. * (w - *b) // different sign since W(c) is -W(c) in our notation
         });
 
-        inverse_inplace(self.buffer1.as_ref(), self.buffer2.as_mut(), &mut self.perm, &mut self.perm_inv);
+        let k_count = inverse_inplace_nodes(self.buffer3.as_ref(), self.buffer2.as_mut(), &mut self.perm, &mut self.perm_inv);
+        // same as in molscat mdprop.f file
 
         zipped!(self.buffer2.as_mut(), eq.unit.as_ref())
         .for_each(|unzipped!(b, u)| {
@@ -181,90 +207,55 @@ impl<R: LogDerivativeReference> LogDerivativeStep<R> {
         });
         // buffer1 is a y_4(a, c)
 
-        R::imbedding1(h, self.w_ref.as_ref(), self.buffer3.as_mut());
-
-        zipped!(self.buffer3.as_mut(), self.buffer2.as_ref())
-        .for_each(|unzipped!(y4, w_tilde)| {
-            *y4 = *y4 + 2. * h / 3. * w_tilde
-        });
-        // buffer3 is a y_1(c, b)
-
-        zipped!(self.buffer1.as_mut(), self.buffer3.as_ref())
-        .for_each(|unzipped!(y4, y1)| {
-            *y4 = *y4 + y1
-        });
-        inverse_inplace(self.buffer1.as_ref(), self.z_matrix.as_mut(), &mut self.perm, &mut self.perm_inv);
-        // z_matrix is a z(a, b, c)
-
-        R::imbedding2(h, self.w_ref.as_ref(), self.buffer1.as_mut());
-        matmul(self.buffer3.as_mut(), self.buffer1.as_ref(), self.z_matrix.as_ref(), None, 1.0, faer::Parallelism::None);
-        R::imbedding3(h, self.w_ref.as_ref(), self.buffer1.as_mut());
-        matmul(self.buffer2.as_mut(), self.buffer3.as_ref(), self.buffer1.as_ref(), None, 1.0, faer::Parallelism::None);
-        // buffer2 is a second term in y_1(a, b)
-        
-        eq.w_matrix(sol.r, &mut self.buffer1);
-        R::imbedding1(h, self.w_ref.as_ref(), self.buffer3.as_mut());
-
-        zipped!(self.buffer3.as_mut(), self.buffer1.as_ref(), self.w_ref.as_ref())
-        .for_each(|unzipped!(y1, w_a, w_ref)| {
-            *y1 = *y1 + h / 3. * (w_ref - w_a) // sign change because of different convention
-        });
-        // buffer3 is a y_1(a, c)
-
-        zipped!(self.buffer3.as_mut(), self.buffer2.as_ref())
-        .for_each(|unzipped!(y1, b)| {
-            *y1 = *y1 - b
-        });
-        // buffer3 is a y_1(a, b)
-        
-        zipped!(self.buffer3.as_mut(), sol.sol.0.as_ref())
-        .for_each(|unzipped!(y1, sol)| {
-            *y1 = sol + *y1
-        });
-        
-        sol.nodes += inverse_inplace_nodes(self.buffer3.as_ref(), sol.sol.0.as_mut(), &mut self.perm, &mut self.perm_inv);
-        // sol is now (y + y1(a, b))^-1
-
-        R::imbedding2(h, self.w_ref.as_ref(), self.buffer1.as_mut());
-        matmul(self.buffer3.as_mut(), self.buffer1.as_ref(), self.z_matrix.as_ref(), None, 1.0, faer::Parallelism::None);
-        matmul(self.buffer2.as_mut(), self.buffer3.as_ref(), self.buffer1.as_ref(), None, 1.0, faer::Parallelism::None);
-
-        matmul(self.buffer1.as_mut(), sol.sol.0.as_ref(), self.buffer2.as_ref(), None, 1.0, faer::Parallelism::None);
-        // buffer1 is now (y + y1(a, b))^-1 * y_2(a, b)
-
-        R::imbedding3(h, self.w_ref.as_ref(), self.buffer2.as_mut());
-        matmul(sol.sol.0.as_mut(), self.buffer2.as_ref(), self.z_matrix.as_ref(), None, 1.0, faer::Parallelism::None);
-        matmul(self.buffer3.as_mut(), sol.sol.0.as_ref(), self.buffer2.as_ref(), None, 1.0, faer::Parallelism::None);
-        
-        matmul(sol.sol.0.as_mut(), self.buffer3.as_ref(), self.buffer1.as_ref(), None, 1.0, faer::Parallelism::None);
-        // sol is now y_3(a, b) * (y + y1(a, b))^-1 * y_2(a, b)
-
-        R::imbedding3(h, self.w_ref.as_ref(), self.buffer1.as_mut());
-        matmul(self.buffer3.as_mut(), self.buffer1.as_ref(), self.z_matrix.as_ref(), None, 1.0, faer::Parallelism::None);
-        R::imbedding2(h, self.w_ref.as_ref(), self.buffer1.as_mut());
-        matmul(self.buffer2.as_mut(), self.buffer3.as_ref(), self.buffer1.as_ref(), None, 1.0, faer::Parallelism::None);
-        // buffer2 is a second term in y_4(a, b)
-        
-        eq.w_matrix(sol.r + sol.dr, &mut self.buffer1);
-        R::imbedding4(h, self.w_ref.as_ref(), self.buffer3.as_mut());
-
-        zipped!(self.buffer3.as_mut(), self.buffer1.as_ref(), self.w_ref.as_ref())
-        .for_each(|unzipped!(y4, w_a, w_ref)| {
-            *y4 = *y4 + h / 3. * (w_ref - w_a) // sign change because of different convention
-        });
-        // buffer3 is a y_4(c, b)
-
-        zipped!(self.buffer3.as_mut(), self.buffer2.as_ref())
-        .for_each(|unzipped!(y4, b)| {
-            *y4 = *y4 - b
-        });
-        // buffer3 is a y_4(a, b)
-
-        zipped!(sol.sol.0.as_mut(), self.buffer3.as_ref())
+        zipped!(sol.sol.0.as_mut(), self.buffer1.as_ref())
         .for_each(|unzipped!(y, y4)| {
             *y = y4 - *y
         });
-        // sol is y(b)
+
+        // sol is Y(c) /////////////////////////////////////
+
+        R::imbedding1(h, self.w_ref.as_ref(), self.buffer1.as_mut());
+
+        zipped!(self.buffer1.as_mut(), self.buffer2.as_ref())
+        .for_each(|unzipped!(y1, w_tilde)| {
+            *y1 = *y1 + 2. * h / 3. * w_tilde
+        });
+        // buffer1 is a y_1(c, b)
+
+        zipped!(self.buffer1.as_mut(), sol.sol.0.as_ref())
+        .for_each(|unzipped!(y1, sol)| {
+            *y1 = sol + *y1
+        });
+        sol.nodes += inverse_inplace_nodes(self.buffer1.as_ref(), sol.sol.0.as_mut(), &mut self.perm, &mut self.perm_inv);
+        // sol is now (Y(c) + y_1(c, b))^-1
+
+        R::imbedding3(h, self.w_ref.as_ref(), self.buffer1.as_mut());
+        matmul(self.buffer2.as_mut(), self.buffer1.as_ref(), sol.sol.0.as_ref(), None, 1.0, faer::Parallelism::None);
+        R::imbedding2(h, self.w_ref.as_ref(), self.buffer1.as_mut());
+        matmul(sol.sol.0.as_mut(), self.buffer2.as_ref(), self.buffer1.as_ref(), None, 1.0, faer::Parallelism::None);
+        // sol is a second term in y_3 (Y(c) + y_1(c, b))^-1 y_2
+
+        eq.w_matrix(sol.r + sol.dr, &mut self.buffer1);
+
+        R::imbedding4(h, self.w_ref.as_ref(), self.buffer2.as_mut());
+
+        zipped!(self.buffer2.as_mut(), self.buffer1.as_ref(), self.w_ref.as_ref())
+        .for_each(|unzipped!(y4, w_b, w_ref)| {
+            *y4 = *y4 + h / 3. * (w_ref - w_b) // sign change because of different convention
+        });
+        // buffer2 is a y_4(c, b)
+
+        zipped!(sol.sol.0.as_mut(), self.buffer2.as_ref())
+        .for_each(|unzipped!(y, y4)| {
+            *y = y4 - *y
+        });
+
+        if sol.nodes < k_count {
+            panic!("Node counting is wrong k-count {} nodes {}", k_count, sol.nodes);
+        }
+        sol.nodes -= k_count;
+
+        // sol is Y(c) 
 
         sol.r += sol.dr;
     }
