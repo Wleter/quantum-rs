8300ad05b38afcadb33107e197f4c8e772509b20
Wleter <marcraftzastepczy@gmail.com>
2025-04-08 12:46:00 +0200

    numerical instabilities during ldlt, lblt decomposition still present

--- benches/benches/inverses.rs
+++ benches/benches/inverses.rs
@@ -1,13 +1,14 @@
 use diol::prelude::*;
 use rand::Rng;
 use scattering_solver::faer::Mat;
-use scattering_solver::utility::{get_inverse_buffer, get_symmetric_inverse_buffer, inverse_inplace, inverse_symmetric_inplace};
+use scattering_solver::utility::{get_lblt_inverse_buffer, get_ldlt_inverse_buffer, get_lu_inverse_buffer, inverse_lblt_inplace, inverse_ldlt_inplace, inverse_lu_inplace};
 
 fn main() -> eyre::Result<()> {
     let mut bench = Bench::new(BenchConfig::from_args()?);
 
     bench.register(bench_inverse_piv_lu, [4, 32, 128, 512, 1024]);
     bench.register(bench_inverse_ldlt, [4, 32, 128, 512, 1024]);
+    bench.register(bench_inverse_lblt, [4, 32, 128, 512, 1024]);
 
     bench.run()?;
     Ok(())
@@ -24,10 +25,10 @@ pub fn setup(size: usize) -> (Mat<f64>, Mat<f64>) {
 
 fn bench_inverse_piv_lu(b: Bencher, size: usize) {
     let (mat, mut out) = setup(size);
-    let mut buffer = get_inverse_buffer(size);
+    let mut buffer = get_lu_inverse_buffer(size);
 
     b.bench(|| {
-        inverse_inplace(mat.as_ref(), out.as_mut(), &mut buffer);
+        inverse_lu_inplace(mat.as_ref(), out.as_mut(), &mut buffer);
 
         black_box(&mut out);
         black_box(&mut buffer);
@@ -36,10 +37,22 @@ fn bench_inverse_piv_lu(b: Bencher, size: usize) {
 
 fn bench_inverse_ldlt(b: Bencher, size: usize) {
     let (mat, mut out) = setup(size);
-    let mut buffer = get_symmetric_inverse_buffer(size);
+    let mut buffer = get_ldlt_inverse_buffer(size);
 
     b.bench(|| {
-        inverse_symmetric_inplace(mat.as_ref(), out.as_mut(), &mut buffer);
+        inverse_ldlt_inplace(mat.as_ref(), out.as_mut(), &mut buffer);
+
+        black_box(&mut out);
+        black_box(&mut buffer);
+    });
+}
+
+fn bench_inverse_lblt(b: Bencher, size: usize) {
+    let (mat, mut out) = setup(size);
+    let mut buffer = get_lblt_inverse_buffer(size);
+
+    b.bench(|| {
+        inverse_lblt_inplace(mat.as_ref(), out.as_mut(), &mut buffer);
 
         black_box(&mut out);
         black_box(&mut buffer);
